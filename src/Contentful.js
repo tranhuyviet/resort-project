049a5a7f820a7aca6fd8d67c431093f29eeaa37b
Viet Tran <tranhuyviet@gmail.com>
2019-10-15 14:48:29 +0300

    add .env file

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "j4tyfy9r58d6",
-  accessToken: "99Ml9Z0iDxHz0y0p2GusXs9IjnrpGOUovBQL68gCN0I"
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
